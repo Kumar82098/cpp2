08db34e11edd4c68cd601b1dd656bf102f687626
Kumar82098 <56931055+Kumar82098@users.noreply.github.com>
2019-10-23 22:02:11 +0530

    upload file

---
+++ matrixmult.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int i,j,k,a[3][3],b[3][3],ab[3][3],sum=0;
+  
+  cout<<endl<<"Enter 9 elements for 1st matrix: "<<endl;
+  for(i=0;i<3;i++)
+   for(j=0;j<3;j++)
+     cin>>a[i][j];
+  
+  cout<<"Enter 9 elements for 2nd matrix : "<<endl;
+  for(i=0;i<3;i++)
+   for(j=0;j<3;j++)
+     cin>>b[i][j];
+     
+  for(i=0;i<3;i++)
+   {
+	   for(j=0;j<3;j++)
+	   {
+	     sum=0;
+	     for(k=0;k<3;k++)
+	      { 
+	      sum=sum+a[i][k]*b[k][j];
+	      }
+         ab[i][j]=sum;
+       }  
+   }
+   cout<<endl<<"Multiplied matrix is:"<<endl;
+   for(i=0;i<3;i++)
+	 for(j=0;j<3;j++)
+	   cout<<ab[i][j]<<" "<<endl;	   
+return 0;
+}   
+ 
